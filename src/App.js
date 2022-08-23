50922c26b09eef1aac17b2838c874136af754985
Reyna Javar <rjavar@catalyte.io>
2022-08-23 14:47:02 -0500

    checks disabled checkbox

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import { useState } from 'react';
-import './App.css';
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("red");
-  const newButtonColor = buttonColor === "red" ? "blue" : "red";
-
+  const [ buttonColor, setButtonColor ] = useState('red');
+  const [ disabled, setDisabled ] = useState(false);
+  
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+  
   return (
     <div>
-      <button
-        style={{ backgroundColor: buttonColor }}
-        onClick={() => setButtonColor(newButtonColor)}
-      >
-        Change to {newButtonColor}
-      </button>
-      <input type="checkbox" id="disable-button-checkbox" />
-    </div>
+    <button
+       style={{backgroundColor: disabled ? 'gray' : buttonColor }}
+       onClick={() => setButtonColor(newButtonColor)}
+       disabled={disabled}
+     >Change to {newButtonColor}</button>
+    <br />
+    <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)} />
+    <label htmlFor="disable-button-checkbox">Disable button</label>
+    </div>  
   );
 }
 

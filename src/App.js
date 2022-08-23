e51c074e545a213a9718d1342495e90f383c689f
Reyna Javar <rjavar@catalyte.io>
2022-08-23 14:21:22 -0500

    write code to meet passing tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
   return (
-    <div className="App">
-      <button style={{ backgroundColor: 'red'}}>Change to blue</button>
+    <div>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        Change to {newButtonColor}
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
+/**
+ * demonstrates functional testing
+ */
 test("button has correct initial color", () => {
   // First render compnent
   render(<App />);
 
   // Find the element we're interested in
   // Find an element with a role of button and text of 'change to blue'
-  const colorButton = screen.getByRole('button', { name: /change to blue/i });
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
 
   // Expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
-});
 
-test("button turns blue when clicked", () => {
-  render(<App />);
-  
-  // Find element you want to click
-  const colorButton = screen.getByRole('button', { name: /change to blue/i });
+  // Click button
+  fireEvent.click(colorButton);
 
-  // TODO: repeating lines of code -> refactor
+  // Expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // Expect the button text to be 'change to red'
+  expect(colorButton.textContent).toBe('Change to red');
 });
